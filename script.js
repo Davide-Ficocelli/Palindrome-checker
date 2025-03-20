fc089a80cef04163eb5733b5469df73ea14f331d
Davide-Ficocelli <davide.ficocelli.dev@gmail.com>
2025-03-20 15:23:02 +0100

    Code and HTML refactoring completed

--- script.js
+++ script.js
@@ -6,11 +6,29 @@ const submitBtn = document.querySelector(".submit-button");
 const form = document.querySelector(".input-form");
 
 // Functions
+
+// Renders an error element with a custom message
 const renderErrElement = (msg) => {
   return `<p class="error-msg">${msg}</p>`;
 };
 
-// Input formatting function
+// Removes punctualization and spaces and sets everything to lowercase in order to be comparable
+function formatToPalindrome(str) {
+  // Removing punctialization and spaces
+  return str
+    .replace(/[^\w\s]|_/g, "")
+    .replaceAll(" ", "")
+    .toLowerCase();
+}
+
+// Reverses the characters' order in a string
+const reverseString = (str) => str.split("").reverse().join("");
+
+// Returns true if a string is a palindrome and false if it isn't
+const checkPalindrome = (originalStr, reversedStr) =>
+  originalStr === reversedStr;
+
+// Main function
 function checkForPalindrome(input) {
   // Selecting previous palidrome results and error message elements
   const previousResult = document.querySelector(".palindrome-result");
@@ -18,8 +36,9 @@ function checkForPalindrome(input) {
 
   // Rendering an error and stopping execution if no input is provided
   if (input.length < 1) {
-    // Remove any previous errors if present
+    // Remove any previous errors and palindrome results if present
     if (previousError) previousError.remove();
+    if (previousResult) previousResult.remove();
 
     return submitBtn.insertAdjacentHTML(
       "afterend",
@@ -32,28 +51,27 @@ function checkForPalindrome(input) {
   if (previousError && input.length >= 1) previousError.remove();
 
   // Removing punctialization and spaces
-  const formattedInput = input
-    .replace(/[^\w\s]|_/g, "")
-    .replaceAll(" ", "")
-    .toLowerCase();
+  const formattedInput = formatToPalindrome(input);
 
   // Reversing the formatted input
-  const reversedInput = formattedInput.split("").reverse().join("");
+  const reversedInput = reverseString(formattedInput);
 
   // Checking whether or not the input is a Palindrome
-  const isPalindrome = reversedInput === formattedInput;
+  const isPalindrome = checkPalindrome(formattedInput, reversedInput);
 
+  // Returning a conditional message depending on the check result
   const resultMsg = isPalindrome
-    ? "It IS a palindrome"
-    : "It is NOT a palindrome";
-  // free
+    ? `"${input}" IS a palindrome ✅`
+    : `"${input}" is NOT a palindrome ❌`;
 
+  // Creating a HTML element containing the conditional message
   const markup = `
     <div class="palindrome-result ${
       isPalindrome ? "is-palindrome" : "not-palindrome"
     }">${resultMsg}</div>
     `;
 
+  // Rendering the newly created HTML element after the form element
   form.insertAdjacentHTML("afterend", markup);
 }
 
